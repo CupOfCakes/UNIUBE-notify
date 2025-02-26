6bd53cd766f9eef93d3039c0211d1b9c587d6d6f
heitor <heitor.scarv@gmail.com>
2025-02-26 14:14:52 -0300

    Update automatic notification.bat

--- automatic notification.bat
+++ automatic notification.bat
@@ -1,5 +1,5 @@
 REM to get the path of python.exe go to cmd and use the command 'where python' if more than one result appears take the first one in the list.
-Rem the 'NotificacaoFacul path' is the path of the file on your computer.
+REM the 'NotificacaoFacul path' is the path of the file on your computer.
 
 @echo off
 *python.exe path* *NotificacaoFacul path*
